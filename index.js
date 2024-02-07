c33a91259cf3eb0fc39da6130f8cf3a94dda9620
Tushar <tushard3998@gmail.com>
2024-02-07 11:46:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+console.log("JavaScript Loops")
+
+let a = 9;
+
+//console.log(a)
+
+/*console.log(a+1)
+console.log(a+2)
+console.log(a+3)*/
+
+//FOR LOOP
+
+/*for (let i = 0; i < 501; i++) {
+    console.log(i)
+}*/
+
+/*for (let index = 1; index < 100; index++) {
+    console.log(index) 
+}*/
+
+//FOR IN 
+
+/*let obj1 = {
+    harry: 91,
+    tushar: 92,
+    lavisha: 93,
+    yash: 94,
+    harsh: 95
+}
+
+for (let a in obj1){
+    console.log("The Marks of " +a +" are " + obj1[a])
+}
+let obj = {
+    name:"Tushar",
+    role:"Programmer",
+    Company:"Coding Ninja"
+}
+
+for (const key in obj) {
+        /*const element = obj[key];
+        console.log(Element)
+}*/
+
+//FOR OF
+
+/*for (const x of "Tushar") {
+    console.log(x)
+}*/
+
+//WHILE LOOP
+
+/*let i=5;
+
+while (i<9) {
+    console.log(i)
+    i++
+}*/
+
+//DO-WHILE
+
+let i=5
+do {console.log(i)
+    i++
+} while (i<9);
